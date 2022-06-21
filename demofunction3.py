f39abd8b10026338d046ccff35c18c2efb7cae7c
yjleeee22 <yeonjilee22@naver.com>
2022-06-21 10:22:35 +0900

    함수 추가

--- demofunction3.py
+++ demofunction3.py
@@ -1,8 +1,3 @@
-# 호출
-print(userURIBuilder("credu.com","80", id="kim", passwd="1234"))
-print(userURIBuilder("credu.com","80", id="kim", passwd="1234",
-    name="mike"))
-
 #람다 함수(간결한 함수 정의)
 g= lambda x,y:x*y
 print(g(3,4))
