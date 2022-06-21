f111f84952d514893d50c06c5d23a47d8d6b4941
yjleeee22 <yeonjilee22@naver.com>
2022-06-21 13:55:45 +0900

    작업중

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,17 @@
+#전역 변수
 str = "Not Class Member"
+
+#클래스를 정의
 class GString:
     def __init__(self):
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        #약간의 실수(버그)
+        print(self.str)
 
+#인스턴스 생성
 g = GString()
 g.set("First Message")
 g.print()
