a68fde270b586e9cc832ac6c3dd8b8b8335de963
yjleeee22 <yeonjilee22@naver.com>
2022-06-22 10:25:03 +0900

    에러 처리 연습

--- try1.py
+++ try1.py
@@ -6,11 +6,11 @@ def divide(a,b):
 # 에러 처리
 try:
     # 함수 호출
-    result = divide(5,0)
+    result = divide(5,2)
 except ZeroDivisionError:
-    print('두번째 인자는 0이어서는 안됩니다')
+    print('0으로 나누면 안됩니다')
 except TypeError:
-    print('모든 인자는 숫자여야 합니다')
+    print('숫자여야 합니다')
 else:
     print("결과:{0}".format(result))
 finally:
