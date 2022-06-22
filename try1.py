d527f15d14bb6cd0dd3014d792c05f03951559d9
yjleeee22 <yeonjilee22@naver.com>
2022-06-22 10:21:51 +0900

    에러 처리

---
+++ try1.py
@@ -0,0 +1,19 @@
+# try1.py
+# 함수 정의
+def divide(a,b):
+    return a/b
+
+# 에러 처리
+try:
+    # 함수 호출
+    result = divide(5,0)
+except ZeroDivisionError:
+    print('두번째 인자는 0이어서는 안됩니다')
+except TypeError:
+    print('모든 인자는 숫자여야 합니다')
+else:
+    print("결과:{0}".format(result))
+finally:
+    print("무조건 실행(한번 더 체크)")
+
+print("---전체 코드 실행 종료---")
