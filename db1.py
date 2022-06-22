1ea881c123fad7326b4de589bb7e97304b856840
yjleeee22 <yeonjilee22@naver.com>
2022-06-22 16:15:02 +0900

    db

--- db1.py
+++ db1.py
@@ -8,10 +8,19 @@ cur = con.cursor()
 # 테이블(스키마)를 생성
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
 # 1건 입력
-cur.execute("insert into PhoneBook values ('Frederick','010-222-3333');")
+cur.execute("insert into PhoneBook values ('Frederick','010-222');")
+# 입력 파라메터 처리
+name = "gildong"
+phoneNumber = "010-123"
+cur.execute("insert into PhoneBook values (?, ?);",(name, phoneNumber))
+# 여러건을 입력
+datalist = (("tom", "010-123"),("Ann","010-452")) 
+#2차원 행렬- 튜플안에 튜플 가능
+cur.executemany("insert into PhoneBook values (?, ?);",datalist)
+
 # 검색
 cur.execute("select * from PhoneBook;")
 
 for row in cur:
-    print(row)
+    print(row[0] + " , " + row[1])
 
