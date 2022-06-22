14fdb482ba9e0a6206a0cdef7fcfe15961ca302c
yjleeee22 <yeonjilee22@naver.com>
2022-06-22 11:19:30 +0900

    파일 연습 완료

--- Demofile.py
+++ Demofile.py
@@ -20,3 +20,33 @@ print("{0:,}".format(1000000))
 print("{0:e}".format(4/3))
 print("{0:f}".format(4/3))
 print("{0:.16f}".format(4/3))
+
+#파일을 생성하기
+f = open("c:\\work\\demo.txt", "wt")
+f.write("첫번째\n두번째\n세번째\n")
+f.close()
+
+#파일을 읽기
+f = open("c:/work/demo.txt","rt")
+result = f.read()
+print(result)
+f.close
+
+#기존 파일에 첨부하는 경우(Append, Read, Write)
+f = open("c:/work/demo.txt","a+")
+f.write("새로운 데이터\n")
+f.close()
+
+#한줄씩 읽어오기
+f = open("c:/work/demo.txt","rt")
+print(f.readline(), end="")
+print(f.readline(), end="")
+print(f.tell())
+f.seek(0)
+lst = f.readlines()
+print(lst)
+f.close()
+
+for item in lst:
+    print(item.replace("\n",""))
+
