43b1842a195efa371d88d322aa5e3c885e4b176d
yjleeee22 <yeonjilee22@naver.com>
2022-06-23 13:18:54 +0900

    크롤링연ㅅ브

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -8,9 +8,12 @@ import re
 # ->헤더가 비어있는지만 체크하므로 적당한 값 채워넣으면 ㅇㅋ(웹봇차단)
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+#파일 생성 : a+ append read write
+f = open("c:\\work\\humor.txt", "a+", encoding="utf-8")
+
+for n in range(1,11):
+        #오늘의 유머 베스트오브 게시판
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -19,20 +22,22 @@ for n in range(0,10):
         #한글이 깨지는 경우는 디코딩 - 사기업들이 만든사이트는 대부분 깨짐
         page = data.decode('utf-8', 'ignore') #약간 깨져도 무시할게
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'}) #attrs-속성들을 지정
+        list = soup.find_all('td', attrs={'class':'subject'}) #attrs-속성들을 지정
 
-# <span class="subject_fixed" data-role="list-title-text">
-# 	아이패드 프로 10.5인치 256기가 와이파이 (전투형, 화이트스팟)
-# </span>
-
-
+#<td class="subject">
+# <a href="/board/view.php?table=bestofbest">
+# 세상은 메타버스라는 스님</a>
+# </td>
 
         for item in list:
                 try:
-                        title = item.text
+                        title = item.find("a").text
                         # print(title.strip())
-                        if (re.search('맥북', title)):
-                                print(title.strip())
+                        if (re.search('누리호', title)):
+                            print(title.strip())
+                            f.write(title.strip() + "\n")
                 except:
                         pass
         
+f.close()
+print("웹 크롤링 종료")
